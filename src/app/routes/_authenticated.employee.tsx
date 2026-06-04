b76a8dc8558333f9b7da4fe6cd439a2b9bed0172
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-06-04 16:28:53 +0200

    feat: implement user routing

---
+++ src/app/routes/_authenticated.employee.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Employee } from '../../pages/Employee';
+
+export const Route = createFileRoute('/_authenticated/employee')({
+  component: Employee,
+});
--- src/app/routes/_authenticated.index.tsx
+++ src/app/routes/_authenticated.index.tsx
@@ -1,6 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { Admin } from '../../pages/Admin';
+import { createFileRoute, Navigate } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authenticated/')({
-  component: () => <Admin />,
+  component: () => <Navigate to="/admin" />,
 });
